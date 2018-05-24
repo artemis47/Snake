830ab768d0b5877343042d00ab13c048803ec120
Yash Rathore <39439875+artemis47@users.noreply.github.com>
2018-05-24 10:06:09 +0530

    Add files via upload

--- Snake.py
+++ Snake.py
@@ -4,17 +4,19 @@ from random import randint
 
 while True:
 
-	stdscr = initscr()
-	start_color()
+	stdscr = initscr() #Initialising Screen
+
+	start_color() #Initialising Default Color Set
 	init_pair(1,COLOR_RED,COLOR_WHITE)
 	init_pair(2,COLOR_WHITE,COLOR_RED)
 	init_pair(3,COLOR_RED,COLOR_GREEN)
-	headX, headY = 0, 0
-	xV, yV = 1, 0
-	appleX, appleY = 0, 0
+
+	headX, headY = 0, 0 #Coordinates of Head of Snake
+	xV, yV = 1, 0 #Horizontal and Vertical velocities
+	appleX, appleY = 0, 0 #Coordinates of "Apple"
 	lx = 0
-	SnakeLen = 3
-	Score = 0
+	SnakeLen = 3 #Initial Length
+	Score = 0 #Initial Score
 
 	ArrX = []
 	ArrY = []
@@ -29,22 +31,24 @@ while True:
 	sleep(1)
 	addstr("Press Any Key To Start\n")
 
-	cbreak()
-	keypad(stdscr,True)
-	noecho()
-	hold = getch()
-	curs_set(0)
+	cbreak() #Accept Input without pressing Enter
+	keypad(stdscr,True) #Accept Keypad Strokes
+	noecho() #Disable printing of KeyStrokes on Screen
+	hold = getch() #Dummy Variable to wait for input before Starting
+	curs_set(0) #Disable Blinking Cursor
 
-	appleX = randint(0,79)
+	appleX = randint(0,79) #Random Position for Apple
 	appleY = randint(0,24)
 	while True:
-		clear()
+		clear() #Clear Screen of previous Output
 
-		mvaddstr(12, 82, "Score = " + str(Score))
+		mvaddstr(12, 82, "Score = " + str(Score)) #Score Counter
 		mvaddstr(13, 82, "Press X to Exit")
-		headX += xV
+		mvaddstr(14, 82, "Press Space to PAUSE")
+		headX += xV #Changing the position of Head of Snake
 		headY += yV
 
+		#Wrap-Around Conditions
 		if headX == -1:
 			headX = 79
 		if headY == -1:
@@ -60,14 +64,26 @@ while True:
 			ArrX[lx] = ArrX[lx + 1]
 			ArrY[lx] = ArrY[lx + 1]
 
-		for lx in range(0, SnakeLen):
-			mvaddch(ArrY[lx], ArrX[lx], ord(' '),color_pair(1))
-			if not ((ArrY[lx] == appleY) and (ArrX[lx] == appleX)):
-				mvaddch(appleY, appleX, ord(' '),color_pair(2))
-		mvaddch(ArrY[SnakeLen], ArrX[SnakeLen], ord(' '),color_pair(3))
-		timeout(1)
+		#Display the Snake's body on screen
+
+		if(has_colors()):
+			for lx in range(0, SnakeLen):
+				mvaddch(ArrY[lx], ArrX[lx], ord(' '),color_pair(1))
+				if not ((ArrY[lx] == appleY) and (ArrX[lx] == appleX)):
+					mvaddch(appleY, appleX, ord(' '),color_pair(2))
+			mvaddch(ArrY[SnakeLen], ArrX[SnakeLen], ord(' '),color_pair(3))
+
+		else: #For Terminals that can't display color
+			for lx in range(0, SnakeLen):
+				mvaddch(ArrY[lx], ArrX[lx], ord('O'))
+				if not ((ArrY[lx] == appleY) and (ArrX[lx] == appleX)):
+					mvaddch(appleY, appleX, ord('@'))
+			mvaddch(ArrY[SnakeLen], ArrX[SnakeLen], ord('X'))
+
+		timeout(1) #Timeout so that snake's movement can carry on
 		cha = getch()
 
+		#Changing Velocity based on input
 		if cha == KEY_UP and yV != 1:
 			yV = -1
 			xV = 0
@@ -80,18 +96,30 @@ while True:
 		elif cha == KEY_RIGHT and xV != -1:
 			yV = 0
 			xV = 1
-		elif cha == ord('x'):
+
+		elif cha == ord('x'): #Exit Condition
 			clear()
 			refresh()
 			break
-		if (headY == appleY) and (headX == appleX):
-			Score += 1
-			appleX = randint(0,79)
+
+		elif cha == ord(' '): #Pause Condition
+			timeout(-1)
+			mvaddstr(14, 82, "PAUSED. Press Space Again to Unpause")
+			cha = getch()
+			while cha != ord(' '):
+				cha = getch()
+				continue
+			timeout(1)
+
+		if (headY == appleY) and (headX == appleX): #When Snake "eats" the apple
+			Score += 1 #Increase Score
+			appleX = randint(0,79) #New Position for Apple
 			appleY = randint(0,24)
 		
-			if SnakeLen < 80:
+			if SnakeLen < 80: #Increase Snake's Length
 				SnakeLen += 1
-		else:
+
+		else: #Conditions for when the Snake "bites" itself
 			if (headX in ArrX[:SnakeLen-1]):
 				if(headY == ArrY[ArrX.index(headX)]):
 					clear()
@@ -102,10 +130,12 @@ while True:
 					clear()
 					refresh()
 					break
-	timeout(-1)
+
+	timeout(-1) #Disabling Timeout and waiting indefinitely for input
 	mvaddstr(12,40, "Your Score is " + str(Score))
 	mvaddstr(13,25, "Press Y To Play Again.Press Any other key to Quit.")
 	hold = getch()
+
 	if hold == ord('y'):
 		clear()
 		refresh()
